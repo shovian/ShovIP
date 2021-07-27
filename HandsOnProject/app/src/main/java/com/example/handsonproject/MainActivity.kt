79494c66da4609b73a549851804eeca7b5004031
shovian <hideyoshishofian@gmail.com>
2021-07-27 14:46:21 +0700

    Bug solved

--- HandsOnProject/app/src/main/java/com/example/handsonproject/MainActivity.kt
+++ HandsOnProject/app/src/main/java/com/example/handsonproject/MainActivity.kt
@@ -4,141 +4,156 @@ package com.example.handsonproject
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.handsonproject.databinding.ActivityMainBinding
 import com.example.handsonproject.databinding.ActivityMainBinding.inflate
 
-
+enum class Operation{
+    PLUS,MINUS,MULTIPLY,DIVIDE,MAIN_STATUS
+}
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var op : Int = 0
+    private var operation : Operation = Operation.MAIN_STATUS
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         binding = inflate(LayoutInflater.from(this))
         setContentView(binding.root)
         with(binding){
-            editText1.hint="angka"
-            textView.text=""
+            stash.text=""
         }
     }
 
 
     fun onTapB1(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "1") }
+        with(binding) { input.setText(input.getText().toString() + "1") }
     }
     fun onTapB2(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "2") }
+        with(binding) { input.setText(input.getText().toString() + "2") }
     }
     fun onTapB3(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "3") }
+        with(binding) { input.setText(input.getText().toString() + "3") }
     }
     fun onTapB4(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "4") }
+        with(binding) { input.setText(input.getText().toString() + "4") }
     }
     fun onTapB5(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "5") }
+        with(binding) { input.setText(input.getText().toString() + "5") }
     }
     fun onTapB6(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "6") }
+        with(binding) { input.setText(input.getText().toString() + "6") }
     }
     fun onTapB7(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "7") }
+        with(binding) { input.setText(input.getText().toString() + "7") }
     }
     fun onTapB8(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "8") }
+        with(binding) { input.setText(input.getText().toString() + "8") }
     }
     fun onTapB9(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "9") }
+        with(binding) { input.setText(input.getText().toString() + "9") }
     }
     fun onTapB0(view: View){
-        with(binding) { editText1.setText(editText1.getText().toString() + "0") }
+        with(binding) { input.setText(input.getText().toString() + "0") }
     }
     fun onTapBEqual(view: View){
         with(binding){
-            if (editText1.text.toString().equals("")&&textView.text.toString().equals("")) return
-            if(op==1){
-                val res=editText1.text.toString().toDouble()+textView.text.toString().toDouble()
-                editText1.setText(res.toString())
-                textView.setText("")
+            if (input.text.toString() == "" && stash.text.toString() ==  "" ) return
+            when(operation){
+                Operation.PLUS -> {
+                    val res=input.text.toString().toDouble()+stash.text.toString().toDouble()
+                    input.setText(res.toString())
+                    stash.setText("")
+                }
+                Operation.MINUS -> {
+                    val res=stash.text.toString().toDouble()-input.text.toString().toDouble()
+                    input.setText(res.toString())
+                    stash.setText("")
+                }
+                Operation.MULTIPLY -> {
+                    val res=input.text.toString().toDouble()*stash.text.toString().toDouble()
+                    input.setText(res.toString())
+                    stash.setText("")
+                }
+                Operation.DIVIDE -> {
+                    val divisor = input.text.toString().toDouble()
+                    val res=stash.text.toString().toDouble()/divisor
+                    if(divisor!=0.0) input.setText(res.toString())
+                    else {
+                        AlertDialog
+                            .Builder(this@MainActivity)
+                            .setTitle("Err")
+                            .setMessage("Division by zero is not allowed!")
+                            .show()
+                    }
+                    stash.setText("")
+                }
             }
-            if(op==2){
-                val res=textView.text.toString().toDouble()-editText1.text.toString().toDouble()
-                editText1.setText(res.toString())
-                textView.setText("")
-            }
-            if(op==3){
-                val res=editText1.text.toString().toDouble()*textView.text.toString().toDouble()
-                editText1.setText(res.toString())
-                textView.setText("")
-            }
-            if(op==4){
-                val res=textView.text.toString().toDouble()/editText1.text.toString().toDouble()
-                editText1.setText(res.toString())
-                textView.setText("")
-            }
-            op=0
+            operation=Operation.MAIN_STATUS
         }
     }
     fun onTapBPlus(view: View){
         with(binding) {
-            if (op==0) {
-                textView.setText(editText1.getText().toString())
-                op=1
+            if (input.text.toString() ==  "" ) return
+            if (operation==Operation.MAIN_STATUS) {
+                stash.setText(input.getText().toString())
+                operation=Operation.PLUS
             }
             else {
                 onTapBEqual(view)
                 onTapBPlus(view)
             }
-            editText1.setText("")
+            input.setText("")
         }
     }
     fun onTapBMinus(view: View){
         with(binding) {
-            if (op==0) {
-                textView.setText(editText1.getText().toString())
-                op=2
+            if (input.text.toString() ==  "" ) return
+            if (operation==Operation.MAIN_STATUS) {
+                stash.setText(input.getText().toString())
+                operation=Operation.MINUS
             }
             else {
                 onTapBEqual(view)
                 onTapBMinus(view)
             }
-            editText1.setText("")
+            input.setText("")
         }
     }
     fun onTapBMultiply(view: View){
         with(binding) {
-            if (op==0) {
-                textView.setText(editText1.getText().toString())
-                op=3
+            if (input.text.toString() ==  "" ) return
+            if (operation==Operation.MAIN_STATUS) {
+                stash.setText(input.getText().toString())
+                operation=Operation.MULTIPLY
             }
             else {
                 onTapBEqual(view)
                 onTapBMultiply(view)
             }
-            editText1.setText("")
+            input.setText("")
         }
     }
     fun onTapBDivide(view: View){
         with(binding) {
-            if (op==0) {
-                textView.setText(editText1.getText().toString())
-                op=4
+            if (input.text.toString() ==  "" ) return
+            if (operation==Operation.MAIN_STATUS) {
+                stash.setText(input.getText().toString())
+                operation=Operation.DIVIDE
             }
             else {
                 onTapBEqual(view)
                 onTapBDivide(view)
             }
-            editText1.setText("")
+            input.setText("")
         }
     }
     fun onTapBClear(view: View){
         with(binding) {
-            textView.setText("")
-            op=0
-            editText1.setText("")
+            stash.setText("")
+            operation=Operation.MAIN_STATUS
+            input.setText("")
         }
     }
 }
